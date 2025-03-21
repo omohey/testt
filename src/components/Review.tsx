7489c6a3595be8b4095c711fddf166203e537577
Omar Moheyeldin <ohafez@noon.com>
2025-03-21 17:50:32 +0200

    reviews component

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,38 +1,77 @@
 import clsx from "clsx";
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft, faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 
-enum ReviewsVariant {
+const MAX_STARS = 5;
+
+export enum ReviewsVariant {
   VERTICAL = "vertical",
   HORIZONTAL = "horizontal",
 }
 
-type TProps = {
-  fontColor: string;
-  bgColor: string;
-  variant: ReviewsVariant;
+export type TReviewItem = {
+  reviewText: string;
+  rating: number;
+  name: string;
+  imgSrc: string;
+  imgAlt?: string;
+  primaryColor?: string;
+  bgColor?: string;
+  variant?: ReviewsVariant;
+  textColor?: string;
 };
-const Review = ({
-  fontColor,
+
+const ReviewItem = ({
+  reviewText,
+  rating,
+  name,
+  imgSrc,
+  imgAlt,
+  primaryColor,
   bgColor,
   variant = ReviewsVariant.VERTICAL,
-}: TProps) => {
+  textColor,
+}: TReviewItem) => {
   return (
     <div
-      className={clsx("flex flex-col", {
-        ["flex-row"]: variant === ReviewsVariant.HORIZONTAL,
+      className={clsx(`flex max-w-[400px] min-w-[200px] justify-center items-center rounded-2xl px-4 py-6 relative gap-2 mx-2`, {
+        ["flex-col pt-[50px] mt-[50px]"]: variant === ReviewsVariant.VERTICAL,
       })}
+      style={{ backgroundColor: bgColor, color: primaryColor }}
     >
-      <img
-        src="https://via.placeholder.com/150"
-        alt="placeholder"
-      />
-      <h5 className="text-lg font-bold">John Doe</h5>
-      <p className="text-sm">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac purus
-        nec diam laoreet sollicitudin."
-      </p>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img loading="lazy" src={imgSrc} alt={imgAlt ?? "Profile"} className={clsx("w-[80px] h-[80px] rounded-full",
+        { ["absolute top-[-40px] right-1/2 translate-x-1/2"]: variant === ReviewsVariant.VERTICAL })} />
+
+      <div className="flex flex-col justify-center items-center gap-2">
+        <h5 className="text-2xl font-bold">{name}</h5>
+        <FontAwesomeIcon icon={faQuoteLeft} className="w-10 !h-10" style={{ color: primaryColor }} />
+        <p className="text-sm text-center text-neutral-500" style={{ ...(textColor ? { color: textColor } : {}) }}>
+          {reviewText}
+        </p>
+        <RatingStars rating={rating} />
+      </div >
     </div>
   );
 };
 
-export default Review;
+export default ReviewItem;
+
+
+const RatingStars = ({ rating }: { rating: number }) => {
+
+  return (
+    <div className="flex space-x-1 text-yellow-500">
+      {[...Array(MAX_STARS)].map((_, index) => {
+        if (index < Math.floor(rating)) {
+          return <FontAwesomeIcon key={index} icon={faStar} className="text-yellow-500 w-4 h-4" />;
+        } else if (index < rating) {
+          return <FontAwesomeIcon key={index} icon={faStarHalfAlt} className="text-yellow-500 w-4 h-4" />;
+        } else {
+          return <FontAwesomeIcon key={index} icon={faStar} className="text-gray-500 w-4 h-4" />;
+        }
+      })}
+    </div>
+  );
+};
---
+++ src/components/ReviewsCarousel.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { useState } from 'react'
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import ReviewItem, { ReviewsVariant, TReviewItem } from './Review';
+import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+type TProps = {
+    title: string;
+    reviews: Array<Omit<TReviewItem, "variant" | "primaryColor" | "bgColor" | "textColor">>;
+    allowTouch?: boolean;
+    isArabic?: boolean;
+    variant?: ReviewsVariant;
+    moduleStyles?: {
+        primaryColor?: string;
+        bgColor?: string;
+        textColor?: string;
+    }
+};
+
+const ReviewsCarousel = ({ title, allowTouch = false, isArabic, reviews, variant = ReviewsVariant.VERTICAL, moduleStyles = { primaryColor: "#152043", bgColor: "#DCDEE7" } }: TProps) => {
+    const [isBeginning, setIsBeginning] = useState(true);
+    const [isEnd, setIsEnd] = useState(false);
+
+
+    const handleSwiperState = (swiper: SwiperClass) => {
+        setIsBeginning(swiper.isBeginning);
+        setIsEnd(swiper.isEnd);
+    }
+
+    return (
+        <div className='flex w-full flex-col gap-4' {...(isArabic ? { dir: "rtl" } : {})}>
+            <div className='flex justify-between items-center'>
+                <h3 className='text-2xl font-bold' style={{ color: moduleStyles?.primaryColor }}>{title}</h3>
+                <div className='flex gap-2'>
+                    <button
+                        className={clsx("prev-button bg-gray-200 w-12 h-12 p-2 rounded-full flex justify-center items-center", { "opacity-50 cursor-not-allowed": isBeginning })}
+                        disabled={isBeginning}
+                    >
+                        <FontAwesomeIcon icon={faArrowLeft} className='text-gray-500 rtl:-rotate-180 w-4 !h-4' />
+                    </button>
+
+                    <button
+                        className={clsx("next-button bg-gray-200 w-12 h-12 p-2 rounded-full flex justify-center items-center", { "opacity-50 cursor-not-allowed": isEnd })}
+                        disabled={isEnd}
+                    >
+                        <FontAwesomeIcon icon={faArrowRight} className='text-gray-500 rtl:-rotate-180 w-4 !h-4' />
+                    </button>
+                </div>
+            </div>
+
+            <div className='w-full'>
+                <Swiper
+                    slidesPerView="auto"
+                    allowTouchMove={allowTouch}
+                    modules={[Navigation]}
+                    onNavigationNext={handleSwiperState}
+                    onNavigationPrev={handleSwiperState}
+                    onSlideChange={handleSwiperState}
+                    onInit={handleSwiperState}
+                    navigation={{
+                        prevEl: ".prev-button",
+                        nextEl: ".next-button",
+                    }}>
+                    {reviews.map((review, index) => (
+                        <SwiperSlide key={index} className='!w-fit'>
+                            <ReviewItem variant={variant} {...moduleStyles} {...review} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+
+        </div>
+    )
+}
+
+export default ReviewsCarousel
